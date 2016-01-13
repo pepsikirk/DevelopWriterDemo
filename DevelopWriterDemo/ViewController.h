79208d1f812d6ae5eb604769ba46f9575ce5683e
pepsikirk <pepsikirk@qq.com>
2016-01-13 18:15:39 +0800

    commit init

---
+++ DevelopWriterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DevelopWriterDemo
+//
+//  Created by jiangxincai on 16/1/13.
+//  Copyright © 2016年 pepsikirk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DevelopWriterDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DevelopWriterDemo
+//
+//  Created by jiangxincai on 16/1/13.
+//  Copyright © 2016年 pepsikirk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
