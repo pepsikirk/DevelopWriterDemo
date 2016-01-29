0fac0f6ddb3ae38979d62678b4be1d20836f4a1f
pepsikirk <pepsikirk@qq.com>
2016-01-29 23:01:05 +0800

    session

--- DevelopWriterDemo/PKShortVideoWriter/PKShortVideoCaptureSession.m
+++ DevelopWriterDemo/PKShortVideoWriter/PKShortVideoCaptureSession.m
@@ -11,11 +11,11 @@
 #import <MobileCoreServices/MobileCoreServices.h>
 #import "PKShortVideoWriter.h"
 
-typedef NS_ENUM( NSInteger, RecordingStatus ) {
-    RecordingStatusIdle = 0,
-    RecordingStatusStartingRecording,
-    RecordingStatusRecording,
-    RecordingStatusStoppingRecording,
+typedef NS_ENUM( NSInteger, PKRecordingStatus ) {
+    PKRecordingStatusIdle = 0,
+    PKRecordingStatusStartingRecording,
+    PKRecordingStatusRecording,
+    PKRecordingStatusStoppingRecording,
 }; 
 
 @interface PKShortVideoCaptureSession() <AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureAudioDataOutputSampleBufferDelegate, PKShortVideoWriterDelegate>
@@ -43,7 +43,7 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
 @property(nonatomic, retain) __attribute__((NSObject)) CMFormatDescriptionRef outputVideoFormatDescription;
 @property(nonatomic, retain) __attribute__((NSObject)) CMFormatDescriptionRef outputAudioFormatDescription;
 
-@property (nonatomic, assign) RecordingStatus recordingStatus;
+@property (nonatomic, assign) PKRecordingStatus recordingStatus;
 
 @property(nonatomic, retain) PKShortVideoWriter *assetWriter;
 
@@ -95,11 +95,10 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
 
 - (void)startRecording {
     @synchronized(self) {
-        if(self.recordingStatus != RecordingStatusIdle) {
-            NSLog(@"Already recording");
+        if(self.recordingStatus != PKRecordingStatusIdle) {
             return;
         }
-        [self transitionToRecordingStatus:RecordingStatusStartingRecording error:nil];
+        [self transitionToRecordingStatus:PKRecordingStatusStartingRecording error:nil];
     }
     
     self.assetWriter = [[PKShortVideoWriter alloc] initWithURL:self.outputFileURL];
@@ -113,10 +112,10 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
 
 - (void)stopRecording {
     @synchronized(self) {
-        if (self.recordingStatus != RecordingStatusRecording){
+        if (self.recordingStatus != PKRecordingStatusRecording){
             return;
         }
-        [self transitionToRecordingStatus:RecordingStatusStoppingRecording error:nil];
+        [self transitionToRecordingStatus:PKRecordingStatusStoppingRecording error:nil];
     }
     [self.assetWriter finishRecording];
 }
@@ -140,13 +139,13 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
                 newCamera = [self cameraWithPosition:AVCaptureDevicePositionFront];
             newInput = [AVCaptureDeviceInput deviceInputWithDevice:newCamera error:nil];
             
-            // beginConfiguration ensures that pending changes are not applied immediately
+            // beginConfiguration 确保改变不会立刻应用
             [self.captureSession beginConfiguration];
             
             [self.captureSession removeInput:input];
             [self.captureSession addInput:newInput];
             
-            // Changes take effect once the outermost commitConfiguration is invoked.
+            // 开始生效
             [self.captureSession commitConfiguration];
             break;
         }
@@ -177,11 +176,11 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
 }
 
 - (void)setCompressionSettings {
-    //    _videoCompressionSettings = [_videoDataOutput recommendedVideoSettingsForAssetWriterWithOutputFileType:AVFileTypeMPEG4];
     NSInteger numPixels = self.outputSize.width * self.outputSize.height;
     NSInteger bitsPerPixel = 6.0;
     NSInteger bitsPerSecond = numPixels * bitsPerPixel;
     
+    // 码率和帧率设置
     NSDictionary *compressionProperties = @{ AVVideoAverageBitRateKey : @(bitsPerSecond),
                                     AVVideoExpectedSourceFrameRateKey : @(30),
                                         AVVideoMaxKeyFrameIntervalKey : @(30) };
@@ -191,7 +190,7 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
                                       AVVideoHeightKey : @(self.outputSize.height),
                        AVVideoCompressionPropertiesKey : compressionProperties };
     
-    //    _audioCompressionSettings = [_audioDataOutput recommendedAudioSettingsForAssetWriterWithOutputFileType:AVFileTypeMPEG4];
+    // 音频设置
     self.audioCompressionSettings = @{ AVEncoderBitRatePerChannelKey : @(28000),
                                    AVFormatIDKey : @(kAudioFormatMPEG4AAC),
                                    AVNumberOfChannelsKey : @(1),
@@ -210,7 +209,7 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
         } else {
             self.outputVideoFormatDescription = formatDescription;
             @synchronized(self) {
-                if(self.recordingStatus == RecordingStatusRecording){
+                if(self.recordingStatus == PKRecordingStatusRecording){
                     [self.assetWriter appendVideoSampleBuffer:sampleBuffer];
                 }
             }
@@ -218,7 +217,7 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
     } else if ( connection == self.audioConnection ){
         self.outputAudioFormatDescription = formatDescription;
         @synchronized( self ) {
-            if(self.recordingStatus == RecordingStatusRecording){
+            if(self.recordingStatus == PKRecordingStatusRecording){
                 [self.assetWriter appendAudioSampleBuffer:sampleBuffer];
             }
         }
@@ -229,58 +228,56 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
 
 - (void)writerDidFinishPreparing:(PKShortVideoCaptureSession *)writer {
     @synchronized(self) {
-        if(self.recordingStatus != RecordingStatusStartingRecording){
-            @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:@"Expected to be in StartingRecording state" userInfo:nil];
+        if(self.recordingStatus != PKRecordingStatusStartingRecording){
             return;
         }
-        [self transitionToRecordingStatus:RecordingStatusRecording error:nil];
+        [self transitionToRecordingStatus:PKRecordingStatusRecording error:nil];
     }
 }
 
 - (void)writer:(PKShortVideoCaptureSession *)writer didFailWithError:(NSError *)error {
     @synchronized( self ) {
         self.assetWriter = nil;
-        [self transitionToRecordingStatus:RecordingStatusIdle error:error];
+        [self transitionToRecordingStatus:PKRecordingStatusIdle error:error];
     }
 }
 
 - (void)writerDidFinishRecording:(PKShortVideoCaptureSession *)writer {
     @synchronized( self ) {
-        if ( self.recordingStatus != RecordingStatusStoppingRecording ) {
-            NSLog(@"Expected to be in StoppingRecording state");
+        if ( self.recordingStatus != PKRecordingStatusStoppingRecording ) {
             return;
         }
     }
     self.assetWriter = nil;
     
     @synchronized( self ) {
-        [self transitionToRecordingStatus:RecordingStatusIdle error:nil];
+        [self transitionToRecordingStatus:PKRecordingStatusIdle error:nil];
     }
 }
 
 
 #pragma mark - Recording State Machine
 
-- (void)transitionToRecordingStatus:(RecordingStatus)newStatus error:(NSError *)error {
-    RecordingStatus oldStatus = self.recordingStatus;
+- (void)transitionToRecordingStatus:(PKRecordingStatus)newStatus error:(NSError *)error {
+    PKRecordingStatus oldStatus = self.recordingStatus;
     self.recordingStatus = newStatus;
     
     if (newStatus != oldStatus){
-        if (error && (newStatus == RecordingStatusIdle)){
+        if (error && (newStatus == PKRecordingStatusIdle)){
             dispatch_async( dispatch_get_main_queue(), ^{
                 @autoreleasepool {
                     [self.delegate session:self didFinishRecordingToOutputFileURL:self.outputFileURL error:error];
                 }
             });
         } else {
-            error = nil; // only the above delegate method takes an error
-            if (oldStatus == RecordingStatusStartingRecording && newStatus == RecordingStatusRecording){
+            error = nil;
+            if (oldStatus == PKRecordingStatusStartingRecording && newStatus == PKRecordingStatusRecording){
                 dispatch_async( dispatch_get_main_queue(), ^{
                     @autoreleasepool {
                         [self.delegate sessionDidBeginRecording:self];
                     }
                 });
-            } else if (oldStatus == RecordingStatusStoppingRecording && newStatus == RecordingStatusIdle) {
+            } else if (oldStatus == PKRecordingStatusStoppingRecording && newStatus == PKRecordingStatusIdle) {
                 dispatch_async( dispatch_get_main_queue(), ^{
                     @autoreleasepool {
                         [self.delegate session:self didFinishRecordingToOutputFileURL:self.outputFileURL error:nil];
@@ -299,11 +296,11 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
     AVCaptureSession *captureSession = [AVCaptureSession new];
     captureSession.sessionPreset = AVCaptureSessionPresetMedium;
     
-    if(![self addDefaultCameraInputToCaptureSession:captureSession]){
-        NSLog(@"failed to add camera input to capture session");
+    if (![self addDefaultCameraInputToCaptureSession:captureSession]){
+        NSLog(@"加载摄像头失败");
     }
-    if(![self addDefaultMicInputToCaptureSession:captureSession]){
-        NSLog(@"failed to add mic input to capture session");
+    if (![self addDefaultMicInputToCaptureSession:captureSession]){
+        NSLog(@"加载麦克风失败");
     }
     
     return captureSession;
@@ -313,38 +310,12 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
     NSError *error;
     AVCaptureDeviceInput *cameraDeviceInput = [[AVCaptureDeviceInput alloc] initWithDevice:[AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo] error:&error];
     
-    if(error){
-        NSLog(@"error configuring camera input: %@", [error localizedDescription]);
+    if (error) {
+        NSLog(@"配置摄像头输入错误: %@", [error localizedDescription]);
         return NO;
     } else {
         BOOL success = [self addInput:cameraDeviceInput toCaptureSession:captureSession];
         self.cameraDevice = cameraDeviceInput.device;
-        return success;
-    }
-}
-
-//Not used in this project, but illustration of how to select a specific camera
-- (BOOL)addCameraAtPosition:(AVCaptureDevicePosition)position toCaptureSession:(AVCaptureSession *)captureSession {
-    NSError *error;
-    NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
-    AVCaptureDeviceInput *cameraDeviceInput;
-    for(AVCaptureDevice *device in devices){
-        if(device.position == position){
-            cameraDeviceInput = [[AVCaptureDeviceInput alloc] initWithDevice:device error:&error];
-        }
-    }
-    if(!cameraDeviceInput){
-        NSLog(@"No capture device found for requested position");
-        return NO;
-    }
-    
-    if(error){
-        NSLog(@"error configuring camera input: %@", [error localizedDescription]);
-        return NO;
-    } else {
-        BOOL success = [self addInput:cameraDeviceInput toCaptureSession:captureSession];
-        self.cameraDevice = cameraDeviceInput.device;
-        [self setFrameRateWithDuration:CMTimeMake(1,30) OnCaptureDevice:cameraDeviceInput.device];
         return success;
     }
 }
@@ -352,8 +323,8 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
 - (BOOL)addDefaultMicInputToCaptureSession:(AVCaptureSession *)captureSession {
     NSError *error;
     AVCaptureDeviceInput *micDeviceInput = [[AVCaptureDeviceInput alloc] initWithDevice:[AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeAudio] error:&error];
-    if(error){
-        NSLog(@"error configuring mic input: %@", [error localizedDescription]);
+    if (error){
+        NSLog(@"配置麦克风输入错误: %@", [error localizedDescription]);
         return NO;
     } else {
         BOOL success = [self addInput:micDeviceInput toCaptureSession:captureSession];
@@ -362,112 +333,24 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
 }
 
 - (BOOL)addInput:(AVCaptureDeviceInput *)input toCaptureSession:(AVCaptureSession *)captureSession {
-    if([captureSession canAddInput:input]){
+    if ([captureSession canAddInput:input]){
         [captureSession addInput:input];
         return YES;
     } else {
-        NSLog(@"can't add input: %@", [input description]);
+        NSLog(@"不能添加输入: %@", [input description]);
     }
     return NO;
 }
 
 
 - (BOOL)addOutput:(AVCaptureOutput *)output toCaptureSession:(AVCaptureSession *)captureSession {
-    if([captureSession canAddOutput:output]){
+    if ([captureSession canAddOutput:output]){
         [captureSession addOutput:output];
         return YES;
     } else {
-        NSLog(@"can't add output: %@", [output description]);
+        NSLog(@"不能添加输出 %@", [output description]);
     }
     return NO;
-}
-
-
-#pragma mark - Methods discussed in the article but not used in this demo app
-
-- (void)setFrameRateWithDuration:(CMTime)frameDuration OnCaptureDevice:(AVCaptureDevice *)device {
-    NSError *error;
-    NSArray *supportedFrameRateRanges = [device.activeFormat videoSupportedFrameRateRanges];
-    BOOL frameRateSupported = NO;
-    for(AVFrameRateRange *range in supportedFrameRateRanges){
-        if(CMTIME_COMPARE_INLINE(frameDuration, >=, range.minFrameDuration) && CMTIME_COMPARE_INLINE(frameDuration, <=, range.maxFrameDuration)){
-            frameRateSupported = YES;
-        }
-    }
-    
-    if(frameRateSupported && [device lockForConfiguration:&error]){
-        [device setActiveVideoMaxFrameDuration:frameDuration];
-        [device setActiveVideoMinFrameDuration:frameDuration];
-        [device unlockForConfiguration];
-    }
-}
-
-
-- (void)listCamerasAndMics {
-    NSLog(@"%@", [[AVCaptureDevice devices] description]);
-    NSError *error;
-    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
-    if(error){
-        NSLog(@"%@", [error localizedDescription]);
-    }
-    [audioSession setActive:YES error:&error];
-    
-    NSArray *availableAudioInputs = [audioSession availableInputs];
-    NSLog(@"audio inputs: %@", [availableAudioInputs description]);
-    for(AVAudioSessionPortDescription *portDescription in availableAudioInputs){
-        NSLog(@"data sources: %@", [[portDescription dataSources] description]);
-    }
-    if([availableAudioInputs count] > 0){
-        AVAudioSessionPortDescription *portDescription = [availableAudioInputs firstObject];
-        if([[portDescription dataSources] count] > 0){
-            NSError *error;
-            AVAudioSessionDataSourceDescription *dataSource = [[portDescription dataSources] lastObject];
-            
-            [portDescription setPreferredDataSource:dataSource error:&error];
-            [self logError:error];
-            
-            [audioSession setPreferredInput:portDescription error:&error];
-            [self logError:error];
-            
-            NSArray *availableAudioInputs = [audioSession availableInputs];
-            NSLog(@"audio inputs: %@", [availableAudioInputs description]);
-            
-        }
-    }
-}
-
-- (void)logError:(NSError *)error {
-    if(error) {
-        NSLog(@"%@", [error localizedDescription]);
-    }
-}
-
-- (void)configureFrontMic {
-    NSError *error;
-    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
-    if(error){
-        NSLog(@"%@", [error localizedDescription]);
-    }
-    [audioSession setActive:YES error:&error];
-    
-    NSArray* inputs = [audioSession availableInputs];
-    AVAudioSessionPortDescription *builtInMic = nil;
-    for (AVAudioSessionPortDescription* port in inputs){
-        if ([port.portType isEqualToString:AVAudioSessionPortBuiltInMic]){
-            builtInMic = port;
-            break;
-        }
-    }
-    
-    for (AVAudioSessionDataSourceDescription* source in builtInMic.dataSources){
-        if ([source.orientation isEqual:AVAudioSessionOrientationFront]){
-            [builtInMic setPreferredDataSource:source error:nil];
-            [audioSession setPreferredInput:builtInMic error:&error];
-            break;
-        }
-    }
 }
 
 - (AVCaptureDevice *)cameraWithPosition:(AVCaptureDevicePosition)position {
@@ -479,7 +362,6 @@ typedef NS_ENUM( NSInteger, RecordingStatus ) {
     }
     return nil;
 }
-
 
 
 #pragma mark - Getter
